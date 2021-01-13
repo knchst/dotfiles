2b88b1f5b6d944b0abc72130695913eccf158e67
Kenichi Saito <knchst@icloud.com>
2021-01-13 17:49:12 +0900

    Update

--- install.sh
+++ install.sh
@@ -11,7 +11,11 @@ cp wwdc17.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 git clone https://github.com/flutter/flutter.git -b stable && mv ./flutter /usr/local/bin
 rm -rf ./flutter
 
+# Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 ./install_dotfile.sh
