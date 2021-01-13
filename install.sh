7e7c4ad85faa2dfaccddb6007d37c2893798b134
Kenichi Saito <knchst@icloud.com>
2021-01-14 00:03:40 +0900

    Update

--- install.sh
+++ install.sh
@@ -6,8 +6,11 @@
 brew tap Homebrew/bundle
 brew bundle
 
+# Xcode color scheme
+mkdir ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 cp wwdc17.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
+# Flutter
 git clone https://github.com/flutter/flutter.git -b stable && mv ./flutter /usr/local/bin
 rm -rf ./flutter
 
@@ -18,4 +21,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
+# nodenv
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+
+# install dotfiles
 ./install_dotfile.sh
